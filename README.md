dc29e9a365137e157dd5429b991598bee2d995a6
Javier Olaechea <pirata@gmail.com>
2018-08-07 21:22:07 -0500

    Barebones version

---
+++ README.md
@@ -0,0 +1,15 @@
+# ri.el
+
+# Usage
+
+`M-x ri`
+
+Or with counsel `M-x counsel-ri`
+
+# License
+
+GPLv3
+
+# Author
+
+Javier Olaechea <pirata@gmail.com>
---
+++ ri.el
@@ -0,0 +1,59 @@
+;;; ri.el --- Browse Ruby API documentation -*- lexical-binding: t -*-
+
+;; Copyright © 2018 Javier Olaechea.
+
+;; Author: Javier Olaechea <pirata@gmail.com>
+;; URL: https://github.com/emacs-pe/ri.el
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1"))
+;; Keywords: help ruby
+
+;; This file is NOT part of GNU Emacs.
+
+;; License
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(defgroup ri nil
+  "Browse Ruby API documentation."
+  :group 'ruby
+  :group 'help
+  :group 'external)
+
+(defcustom ri-program-name "ri"
+  "The path where the ri executable is located."
+  :type 'string
+  :group 'ri)
+
+;; XXX: Should I create a buffer per class instead? e.j. *ri Array*?
+(defcustom ri-process-buffer "*ri*"
+  "The name of the buffer where ri outputs the documentation."
+  :group 'ri)
+
+
+;;;###autoload
+(defun ri (class-name)
+  (interactive "MClass-name: ")
+  (with-current-buffer (get-buffer-create ri-process-buffer)
+    (display-buffer (current-buffer))
+    (erase-buffer)
+    (insert (shell-command-to-string (format "%s --no-pager --format=ansi %s" ri-program-name class-name)))
+    (ansi-color-apply-on-region (point-min) (point-max))
+    (beginning-of-buffer)))
+
+(provide 'ri)
